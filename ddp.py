220902ae810920fac3069ee589f127fd15effa9a
angkul07 <angkul58@gmail.com>
2025-05-20 17:37:11 +0530

    setup ddp and ddp sampler

--- ddp.py
+++ ddp.py
@@ -1,23 +1,40 @@
+import os
 import torch
 import torch.distributed as dist
 from torch.utils.data.distributed import DistributedSampler
 
-def setup(rank: int, world_size: int) -> None:
+def setup_ddp():
+    if 'RANK' not in os.environ or 'WORLD_SIZE' not in os.environ:
+        os.environ['RANK'] = os.environ.get('LOCAL_RANK', '0')
+        os.environ['WORLD_SIZE'] = str(torch.cuda.device_count())
+
+def init_ddp(rank, world_size) -> None:
     """Initialize the distributed environment."""
+
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '29500'
+
     dist.init_process_group(
         backend="nccl",
-        init_method="env://",
         rank=rank,
         world_size=world_size
     )
     torch.cuda.set_device(rank)
-    torch.distributed.barrier()  # Ensure all processes are synchronized
 
-def prepare(rank, world_size, batch_size, dataset, pin_memory=False, num_workers=0):
+def is_main_process(rank) -> bool:
+    return rank == 0
+
+def ddp_sampler(dataset, world_size, rank, shuffle=True, drop_last=False):
     """Prepare the DataLoader for distributed training."""
-    sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=True, drop_last=False)
-
+    sampler = DistributedSampler(
+        dataset,
+        num_replicas=world_size,
+        rank=rank,
+        shuffle=shuffle,
+        drop_last=drop_last
+    )
     return sampler
 
 def cleanup() -> None:
-    dist.destroy_process_group()
+    if dist.is_initialized():
+        dist.destroy_process_group()
