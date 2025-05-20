c527d3bcb6c81c2dc014942ebdf0f3356be1f652
angkul07 <angkul58@gmail.com>
2025-05-20 17:37:30 +0530

    init ddp, model saving

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
+import os
 import torch
 import torch.optim as optim
-import torch.multiprocessing as mp
+import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel
 import math
 from tqdm import tqdm
@@ -8,13 +9,12 @@ from datasets import load_from_disk, DatasetDict
 
 from sample import generate_text
 from model import GPTModel
-from preprocess import create_dataloader
+from preprocess import create_dataloader, TextChunkDataset
 from tokenizer import Tokenizer
 from plot import plot_losses
-from ddp import setup, cleanup
+from ddp import setup_ddp, init_ddp, ddp_sampler, cleanup, is_main_process
 import config
 
-is_ddp = config.DDP
 DEVICE = config.DEVICE
 
 def calculate_loss(logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
@@ -23,7 +23,7 @@ def calculate_loss(logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
 
 @torch.no_grad()
 def calculate_average_loss(data_loader: torch.utils.data.DataLoader, model: GPTModel, 
-                           device: DEVICE, num_batches: int | None = None) -> float:
+                           device= DEVICE, num_batches: int | None = None, is_ddp=False, world_size=1) -> float:
     """Calculates average loss over a data loader."""
     model.eval()
     total_loss = 0.0
@@ -46,7 +46,12 @@ def calculate_average_loss(data_loader: torch.utils.data.DataLoader, model: GPTM
         input_batch, target_batch = input_batch.to(device), target_batch.to(device)
         logits = model(input_batch)
         loss = calculate_loss(logits, target_batch)
-        total_loss += loss.item()
+        if is_ddp:
+            reduced_loss = loss.detach().clone()
+            dist.reduce(reduced_loss, dst=0, op=dist.ReduceOp.SUM)
+            total_loss += reduced_loss.item() / world_size
+        else:
+            total_loss += loss.item()
         actual_batches += 1
     
     model.train()
@@ -69,67 +74,74 @@ def get_lr(it: int, warmup_steps: int, total_training_steps: int,
 
 
 def train_model():
-    torch.manual_seed(123)
-    device = DEVICE
-    setup(rank=config.RANK, world_size=config.WORLD_SIZE) if is_ddp else None  
+    local_rank = int(os.environ.get("LOCAL_RANK", -1))
+    world_size = int(os.environ.get("WORLD_SIZE", -1))
 
-    try:
-        tokenizer = Tokenizer()
-        if config.GPT_CONFIG["vocab_size"] != tokenizer.vocab_size:
-            print(f"Warning: Config vocab_size ({config.GPT_CONFIG['vocab_size']}) "
-                  f"differs from tokenizer vocab_size ({tokenizer.vocab_size}). "
-                  f"Using tokenizer's vocab_size for the model.")
-            
-    except FileNotFoundError as e:
-        print(f"Error: {e}")
-        print("Please run train_tokenizer.py first.")
-        return
-    
+    is_ddp = local_rank != -1 and world_size > 1
+
     if is_ddp:
-        model = GPTModel(config.GPT_CONFIG).to(config.RANK)
-        model = DistributedDataParallel(model, device_ids=[config.RANK], output_device=config.RANK)
+        setup_ddp()
+        init_ddp(local_rank, world_size)
+        device = local_rank
     else:
-        model = GPTModel(config.GPT_CONFIG).to(device)
-    print(f"Model created with {sum(p.numel() for p in model.parameters())/1e6:.2f}M parameters.")
+        local_rank = 0
+        world_size = 1
+        device = DEVICE
+        is_ddp = False
 
-    try:
-        combined_hf_dataset = load_from_disk(str(config.COMBINED_DATASET_PATH))
-    except FileNotFoundError:
-        print(f"Error: Combined dataset not found at {config.COMBINED_DATASET_PATH}")
-        print("Please run Datasets.py (create_combined_dataset) first.")
-        return
+    if is_main_process(local_rank):
+        print(f"Running on device: {device}. DDP active: {is_ddp}. World size: {world_size}")
+
+    torch.manual_seed(123+local_rank)
+
+    tokenizer = Tokenizer()
+    
+    model = GPTModel(config.GPT_CONFIG).to(device)
+
+    if is_ddp:
+        model = DistributedDataParallel(model, device_ids=[local_rank], output_device=local_rank, find_unused_parameters=False)
+
+    # Dataset Loading -->
+    combined_hf_dataset = load_from_disk(str(config.COMBINED_DATASET_PATH))
 
     if not isinstance(combined_hf_dataset, DatasetDict):
         if hasattr(combined_hf_dataset, 'shuffle'):
             combined_hf_dataset = combined_hf_dataset.shuffle(seed=42)
-        split_data = combined_hf_dataset.train_test_split(test_size=0.1, seed=123)
+        split_data = combined_hf_dataset.train_test_split(test_size=0.1, seed=123+local_rank)
     else:
         split_data = combined_hf_dataset
 
     train_texts = split_data['train']['text']
     val_texts = split_data['test']['text']
 
+    train_set = TextChunkDataset(train_texts, tokenizer, config.GPT_CONFIG['context_length'], config.GPT_CONFIG['context_length']//2)
+    val_set = TextChunkDataset(val_texts, tokenizer, config.GPT_CONFIG['context_length'], config.GPT_CONFIG['context_length']//2)
+
+    train_sampler = ddp_sampler(train_set, world_size=world_size, rank=local_rank)
+    val_sampler = ddp_sampler(val_set, world_size=world_size, rank=local_rank)
+
     train_loader = create_dataloader(
-        train_texts, tokenizer,
+        train_set,
         batch_size=config.BATCH_SIZE,
-        max_length=config.GPT_CONFIG['context_length'],
-        stride=config.GPT_CONFIG['context_length'] // 2, # 50% overlap: for smaller datasets else stride=config.GPT_CONFIG['context_length']
-        shuffle=True, drop_last=True, num_workers=2
+        shuffle=True, drop_last=True,
+        sampler=train_sampler
     )
     val_loader = create_dataloader(
-        val_texts, tokenizer,
+        val_set,
         batch_size=config.BATCH_SIZE,
-        max_length=config.GPT_CONFIG['context_length'],
-        stride=config.GPT_CONFIG['context_length'] // 2,
-        shuffle=False, drop_last=False, num_workers=2
+        shuffle=True, drop_last=True,
+        sampler=val_sampler
     )
     
-    if len(train_loader) == 0 or len(val_loader) == 0:
+    if len(train_loader) == 0 or len(val_loader) == 0 and is_main_process(local_rank):
         print("Error: One or both dataloaders are empty. Check dataset processing.")
+        if is_ddp: 
+            cleanup()
         return
 
+    optimizer_model = model.module if is_ddp else model
     optimizer = optim.AdamW(
-        model.parameters(), 
+        optimizer_model.parameters(), 
         lr=config.LEARNING_RATE, # Peak LR
         weight_decay=config.WEIGHT_DECAY
     )
@@ -179,8 +191,8 @@ def train_model():
 
             # Logging and Evaluation
             if global_step % config.EVAL_FREQ == 0:
-                avg_train_loss = calculate_average_loss(train_loader, model, device, num_batches=config.EVAL_ITER)
-                avg_val_loss = calculate_average_loss(val_loader, model, device, num_batches=config.EVAL_ITER)
+                avg_train_loss = calculate_average_loss(train_loader, model, device, num_batches=config.EVAL_ITER, is_ddp=is_ddp, world_size=world_size)
+                avg_val_loss = calculate_average_loss(val_loader, model, device, num_batches=config.EVAL_ITER, is_ddp=is_ddp, world_size=world_size)
                 train_losses.append(avg_train_loss)
                 val_losses.append(avg_val_loss)
                 
@@ -191,12 +203,13 @@ def train_model():
                 })
 
         print(f"\nEpoch {epoch+1} completed.")
-        avg_epoch_train_loss = calculate_average_loss(train_loader, model, device)
-        avg_epoch_val_loss = calculate_average_loss(val_loader, model, device)
+        avg_epoch_train_loss = calculate_average_loss(train_loader, model, device, is_ddp=is_ddp, world_size=world_size)
+        avg_epoch_val_loss = calculate_average_loss(val_loader, model, device, is_ddp=is_ddp, world_size=world_size)
         print(f"End of Epoch {epoch+1} Train Loss: {avg_epoch_train_loss:.3f} Val Loss: {avg_epoch_val_loss:.3f}")
 
+        actual_model_for_gen = model.module if is_ddp else model
         sample_output = generate_text(
-            model=model,
+            model=actual_model_for_gen,
             tokenizer=tokenizer,
             start_text="You are a helpful assistant",
             max_new_tokens=config.SAMPLE_MAX_NEW_TOKENS,
@@ -211,7 +224,7 @@ def train_model():
 
         # --- Save Checkpoint ---
         checkpoint = {
-            "model_state_dict": model.state_dict(),
+            "model_state_dict": model.module.state_dict() if is_ddp else model.state_dict(),
             "optimizer_state_dict": optimizer.state_dict(),
             "epoch": epoch,
             "global_step": global_step,
@@ -227,10 +240,14 @@ def train_model():
         torch.save(model.module.state_dict(), config.MODEL_CHECKPOINT_SAVE_PATH)
         cleanup()
 
-    print("Training complete.")
-    # save the final model's weights only for easier deployment/sharing
-    torch.save(model.state_dict(), config.FINAL_MODEL_SAVE_PATH)
-    print(f"Final model weights saved to {config.FINAL_MODEL_SAVE_PATH}")
+    if is_main_process(local_rank):
+        print("Training complete.")
+        final_model_state = model.module.state_dict() if is_ddp else model.state_dict()
+        torch.save(final_model_state, str(config.FINAL_MODEL_SAVE_PATH))
+        print(f"Final model weights saved to {config.FINAL_MODEL_SAVE_PATH}")
+    
+    if is_ddp:
+        cleanup()
 
     plot_losses(
     train_losses=train_losses,
@@ -240,7 +257,4 @@ def train_model():
     )   
 
 if __name__ == "__main__":
-    if is_ddp:
-        mp.spawn(train_model, nprocs=config.NUM_GPUS, join=True)
-    else:
-        train_model()
+    train_model()
